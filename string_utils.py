e9d556de04ca1ded30f9e7e100923237e8a0a8f9
naamabtov <naamabtov@gmail.com>
2025-11-23 17:31:48 +0200

    Correct atom count assignment in dictionary
    Fix syntax error in atom count assignment.

--- string_utils.py
+++ string_utils.py
@@ -34,13 +34,16 @@ def count_atoms_in_molecule(molecular_formula):
     Example: 'H2O' → {'H': 2, 'O': 1}"""
 
     # Step 1: Initialize an empty dictionary to store atom counts
+    dic={}
 
     for atom in split_by_capitals(molecular_formula):
         atom_name, atom_count = split_at_number(atom)
         
         # Step 2: Update the dictionary with the atom name and count
+         dic[atom_name]= atom count
 
     # Step 3: Return the completed dictionary
+     return dic
 
 
 
